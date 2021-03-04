9c6e5cc97d93f7a6cb208beb9bc9b504ed8d5ef9
Sebi <stoicescusebastian@yahoo.com>
2021-03-04 12:40:59 +0200

    feat: add bcrypt

--- src/api/users/logic.js
+++ src/api/users/logic.js
@@ -1,8 +1,12 @@
 const database = require('./database')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 module.exports = {
     getAll: () => database.getAll(),
     create: user => {
+        let hash = bcrypt.hashSync(user.password, saltRounds)
+        user.password = hash 
         return database.create(user)
     }
 }

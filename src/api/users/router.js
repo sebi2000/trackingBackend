9ea3ca5218674ea515cfad98f13fd50e262d3fdc
Sebi <stoicescusebastian@yahoo.com>
2021-03-04 11:11:43 +0200

    chore: fix identation

--- src/api/users/router.js
+++ src/api/users/router.js
@@ -5,13 +5,12 @@ const bcrypt = require('bcrypt')
 
 router.route('/')
     .post((req, res) =>{ 
-
-            logic.create(req.body).then(users => {
-                res.json(users)      
-            })
-            .catch(err => {
-                res.send(err)
-            })  
+        logic.create(req.body).then(users => {
+            res.json(users)      
+        })
+        .catch(err => {
+            res.send(err)
+        })  
     })
     .get((req,res) => {
         logic.getAll().then(users => {

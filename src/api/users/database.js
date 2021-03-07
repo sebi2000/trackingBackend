ecdd489963a8af07100e85ba908f2641a0176bba
Barbulescu Maria-Bianca <62563948+barbulescumb@users.noreply.github.com>
2021-03-07 11:04:30 +0200

    refactor:hardcoded_admin_again

--- src/api/users/database.js
+++ src/api/users/database.js
@@ -1,11 +1,5 @@
 const users = require('../../database/models/users')
 
-state={
-    admins:[
-        {email: "admin@gmail.com",
-         password : "admin"}
-    ]
-}
 module.exports = {
     getAll: () => users.find({}).lean().exec(),
     getById: id => users.findById(id).lean().exec(),

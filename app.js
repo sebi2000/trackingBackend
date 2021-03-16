be2d32659c3281fe3cfaa959ff612a1c8ef2d252
Andreea <muntean.anda2000@gmail.com>
2021-03-16 14:57:40 +0200

    chore: delete comments

--- app.js
+++ app.js
@@ -33,11 +33,6 @@ connection().then( () => {
    app.use('/users', usersRouter)
    app.use('/auth', authRouter)
    app.use('/entries', entriesRouter)
-  //  app.use('/', function(req, res) {
-  //    res.send('working')
-  //  })
-     
-  // catch 404 and forward to error handler
     app.use(function(req, res, next) {
       next(createError(404))
     })

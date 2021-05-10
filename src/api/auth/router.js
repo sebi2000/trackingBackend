493841b20ec79259c06768c584eee684521fc19e
Sebi <stoicescusebastian@yahoo.com>
2021-05-10 16:00:52 +0300

    chore : delete console log

--- src/api/auth/router.js
+++ src/api/auth/router.js
@@ -12,7 +12,6 @@ router.route('/')
         if(!errors.isEmpty())
             res.json(Helpers.handleResponse(errors, StatusCodes.UNPROCESSABLE_ENTITY))
         else logic.login(req.body.user).then(resp => {
-            console.log(resp)
             if(resp.userFound)
                 req.session.user = req.body.user
             res.send(resp)

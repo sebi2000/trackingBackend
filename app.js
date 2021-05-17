9dafd07eecca8d6290b0de3e1ef82fe906fc071e
Sebi <stoicescusebastian@yahoo.com>
2021-05-17 12:27:04 +0300

    feat : add jwt auth

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser')
+<<<<<<< HEAD
 const session = require('express-session')
 const CronJob = require('cron').CronJob
 const {sendEmailToAdmin} = require('./src/utils/services')
+=======
+const session = require('express-session');
+const jwt = require('jsonwebtoken')
+const { getById } = require('./src/api/users/logic')
+>>>>>>> c4d0148 (feat : add jwt auth)
 
 const CONSTANTS = require('./src/utils/constants')
 const connection = require('./src/database/connection')
@@ -16,6 +22,26 @@ const usersRouter = require('./src/api/users/router')
 const authRouter = require('./src/api/auth/router')
 const entriesRouter = require('./src/api/entries/router')
 const resetRouter = require('./src/api/reset/router')
+
+const authHandler = async (req, res, next) => {
+  let auth = req.headers.authorization
+  token = auth.substr(auth.indexOf(' ') + 1)
+  let decoded
+
+  try{
+    decoded = jwt.verify(token, 'secret')
+    req.auth = decoded
+  }catch(err) {
+    res.send(err)
+  }
+
+  let user = await getById(decoded.user._id)
+ 
+  if(user !== null)
+    next()
+  else res.send('User not found')
+  
+}
 
 const app = express();
 
@@ -35,8 +61,9 @@ connection().then( () => {
     secret: 'maresecret',
   }))
 
-  app.use('/reset', resetRouter)
   app.use('/auth', authRouter)
+  app.use(authHandler)
+  app.use('/reset', resetRouter)
   app.use('/users', usersRouter)
   app.use('/entries', entriesRouter)
 
@@ -47,13 +74,19 @@ connection().then( () => {
 
 
   app.use('/isLogged', (req, res) => {
-    res.send(req.session.user)
+    res.send(req.auth)
   })
 
   app.use('/logout', (req, res) => {
-    req.session.destroy()
-    res.send("Logged out")
+    delete req.auth
+    console.log("REQ.AUTH", req.auth)
+    res.send("Log out")
+
   })
+
+  
+
+  
  
   app.use(function(req, res, next) {
       next(createError(404))
@@ -69,7 +102,6 @@ connection().then( () => {
       res.status(err.status || 500);
       res.send(err)
     })
-
 
     app.listen(CONSTANTS.PORT)
     console.log(`Listening to port ${CONSTANTS.PORT} and connected to database`)
--- src/api/auth/logic.js
+++ src/api/auth/logic.js
@@ -1,8 +1,9 @@
 const database = require ('../users/database')
 const bcrypt = require('bcrypt')
-const { MESSAGES: { USER_NOT_FOUND, AUTH_SUCCESS, INCORRECT_PASS } } = require('../../utils/constants')
+const { MESSAGES: { USER_NOT_FOUND, AUTH_SUCCESS, INCORRECT_PASS }, EXP_TIME} = require('../../utils/constants')
 const Helpers = require('../../utils/helpers')
 const { StatusCodes } = require('http-status-codes')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
     login : async user => {
@@ -11,7 +12,12 @@ module.exports = {
             return Helpers.handleResponse(USER_NOT_FOUND, StatusCodes.FORBIDDEN)
         }
         else if(bcrypt.compareSync(user.password, userFound.password)){
-            return {userFound} 
+            
+            let token = jwt.sign({
+                user: userFound
+            }, 'secret', { expiresIn: EXP_TIME })
+
+            return {userFound, token} 
         }  
         else{
             return Helpers.handleResponse(INCORRECT_PASS, StatusCodes.FORBIDDEN)
